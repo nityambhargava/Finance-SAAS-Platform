816461f839a7707165fc8ffeef0707aaf2ad43c7
nityambhargava <nityambhargava@gmail.com>
2024-09-15 20:22:35 +0530

    Updated next.config.mjs for trailing slash

--- Documents/Projects/finance-application/next.config.mjs
+++ Documents/Projects/finance-application/next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    trailingSlash: true, //Edited this line
+};
 
 export default nextConfig;
+
+module.exports = {                          //Edited line 8 to 18
+    async redirects() {
+      return [
+        {
+          source: '/old-path',
+          destination: '/new-path',
+          permanent: true,
+        },
+      ]
+    },
+  }
+  
